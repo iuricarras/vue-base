69b5093d1f665e48c1e91b163b1d3130354ade29
Iuri Carrasqueiro <iuri.carrasqueiro@gmail.com>
2026-03-12 14:33:05 +0000

    First Commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Button } from '@/components/ui/button';
+
+</script>
+
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <Button >Click Me</Button>
+    </div>
+</template>
